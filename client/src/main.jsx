e75ada41b2f3b4bd3dfb674ba95ad286c1f893ac
Chakshu Joshi <81384685+Chakshu1221@users.noreply.github.com>
2026-08-14 08:24:09 +0530

    css fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import App from './App.jsx';
-import './pages/css/responsive.css';
+import './pages/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
